1bc1a459eb17b4dfc36cc7f268d9e726da8a9b9e
Luis Giraldo <lugirva@gmail.com>
2020-08-24 09:39:18 -0400

    Removed markup folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {    
+const getCourses = () => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", 'http://localhost:5000/api/courses');
+    xhr.onload = () => {
+        if(xhr.status === 200){
+            const data = JSON.parse(xhr.responseText);
+            resolve(data);
+        } else {
+            reject(Error (xhr.statusText));
+        }
+    };
+    xhr.onerror = () => reject(Error("A network error ocurred!"));
+    xhr.send();          
+  });//end Promise    
+};//end getCourses
+
+
+function App() {  
+  // const coursesList = getCourses();
+  // console.log(coursesList);
+  const [courses, setCourses] = useState(['none']);
+  useEffect( () => {
+    getCourses('http://localhost:5000/api/courses')
+      .then(data => setCourses(data.Courses))
+      .catch(err => console.log('Oh noes!', err));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +44,15 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          { courses.map( course => {
+            const id = course.id ? course.id.toString() : "";
+            return <li key={id}>{ course.title }</li>
+          })}
+        </ul>
       </header>
+      <p>
+      </p>
     </div>
   );
 }
