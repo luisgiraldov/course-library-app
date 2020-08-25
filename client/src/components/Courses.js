24c02f51853fe3769b8f056304973168bfb89a97
Luis Giraldo <lugirva@gmail.com>
2020-08-25 18:22:01 -0400

    Added styles to Course component

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -34,6 +34,13 @@ const Courses = () => {
 
     return(
         <div>
+            <div className="hero-background">
+            </div>
+            <div className="hero-text-container">
+                    <h2>Welcome to the Course Library</h2>
+                    <p>This application will provide a way for users to administer a school database containing information about courses: users can interact with the database by retrieving a list of courses, viewing detail for a specific course, as well as creating, updating and deleting courses in the database.  In addition, the project will require users to create an account and sign in to make changes to the database.</p>                  
+                    <button className="goToBtn">Go to courses</button>
+            </div>
             <ul className="tilesWrap">
                 {
                     courses.map( course => {
@@ -47,20 +54,13 @@ const Courses = () => {
                                 </li>
                     })
                 }
-            </ul>
-            <div className="add-course-container">
-                <ul className="tilesWrap">
-                    <li>
-                        <div className="desktop"></div>
-                    </li>
-                    <li key={ "newCourse" }>
+                <li key={ "newCourse" }>
                             <h2>+</h2>
                             <h3>Add New Course</h3>
                             <p>Ready with your new class, create your new course here!</p>
                             <button>Add</button>
-                    </li>
-                </ul>
-            </div>
+                </li>
+            </ul>
         </div>
     );
 };
