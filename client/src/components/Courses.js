ff97e6834d95bd6da42d652d3309fd5f66ce0d4b
Luis Giraldo <lugirva@gmail.com>
2020-09-28 09:32:35 -0400

    Commented out 'Go to courses' button to implement react-scroll later

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -28,7 +28,7 @@ const Courses = ({ context }) => {
             <div className="hero-text-container">
                     <h2>Welcome to the Course Library</h2>
                     <p>This application will provide a way for users to administer a school database containing information about courses: users can interact with the database by retrieving a list of courses, viewing detail for a specific course, as well as creating, updating and deleting courses in the database.  In addition, the project will require users to create an account and sign in to make changes to the database.</p>                  
-                    <button className="goToBtn">Go to courses</button>
+                    {/* <button className="goToBtn">Go to courses</button> */}
             </div>
             <ul className="tilesWrap">
                 {
